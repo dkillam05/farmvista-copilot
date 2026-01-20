269eff6a4832a786dc701c699598132d569626cd
dkillam05 <danekillam@gmail.com>
2026-01-20 14:32:59 -0600

    Create index.js

---
+++ src/data/getters/index.js
@@ -0,0 +1,5 @@
+// /src/data/getters/index.js  (FULL FILE)
+// Rev: 2026-01-20-v2-getters-index
+
+export { getFieldFullByKey } from './fields.js';
+export { getGrainBagsDownSummary } from './grainBags.js';
