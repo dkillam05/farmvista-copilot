da50a4e60acee37dfeb4af0febc029f799fa97f1
dkillam05 <danekillam@gmail.com>
2026-01-21 06:05:56 -0600

    Update index.js

--- src/data/getters/index.js
+++ src/data/getters/index.js
@@ -1,6 +1,13 @@
 // /src/data/getters/index.js  (FULL FILE)
-// Rev: 2026-01-20-v2-getters-index-rtk
+// Rev: 2026-01-21-v2-getters-index-rtk-all
+//
+// Exports all RTK getters from rtkTowers.js (single RTK file rule)
 
 export { getFieldFullByKey } from './fields.js';
 export { getGrainBagsDownSummary } from './grainBags.js';
-export { getRtkTowerCount } from './rtkTowers.js';
+
+export {
+  getRtkTowerCount,
+  getRtkTowerList,
+  getFieldsByRtkTowerKey
+} from './rtkTowers.js';
