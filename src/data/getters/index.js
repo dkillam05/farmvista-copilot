fb0b02ba35c1ab1171880af5df7f680d5a7ef028
dkillam05 <danekillam@gmail.com>
2026-01-22 14:35:40 -0600

    Update index.js

--- src/data/getters/index.js
+++ src/data/getters/index.js
@@ -1,5 +1,11 @@
 // /src/data/getters/index.js  (FULL FILE)
-// Rev: 2026-01-21-v2-getters-index-active-default-county-suite
+// Rev: 2026-01-22-v3-getters-index-add-new-domains
+//
+// Keeps existing exports and adds:
+// - boundaryRequests
+// - fieldMaintenance
+// - equipment / equipmentMakes / equipmentModels
+// - binSites / binMovements
 
 export { getFieldFullByKey } from './fields.js';
 export { getGrainBagsDownSummary } from './grainBags.js';
@@ -16,3 +22,15 @@ export {
   getFieldsInCounty,
   getFarmsInCounty
 } from './counties.js';
+
+// NEW GETTERS
+export { getBoundaryRequests } from './boundaryRequests.js';
+
+export { getFieldMaintenance } from './fieldMaintenance.js';
+
+export { getEquipment } from './equipment.js';
+export { getEquipmentMakes } from './equipmentMakes.js';
+export { getEquipmentModels } from './equipmentModels.js';
+
+export { getBinSites } from './binSites.js';
+export { getBinMovements } from './binMovements.js';
